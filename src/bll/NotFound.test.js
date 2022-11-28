88c20ff7be92f3831b3eb5f87cf53dce71bd8491
alexsharich <alexsharich@gmail.com>
2022-11-28 15:21:49 +0100

    add enzyme, NotFound.test.js

---
+++ src/bll/NotFound.test.js
@@ -0,0 +1,41 @@
+import Enzyme,{shallow,render,mount} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import toJson from 'enzyme-to-json'
+import { NotFound404 } from '../ui/components/main/notFound404/NotFound404'
+
+
+Enzyme.configure({adapter:new Adapter()})
+
+global.shallow = shallow
+global.render = render
+global.mount = mount
+global.toJson = toJson
+
+console.error = message=>{
+  throw new Error(message)
+}
+
+const getComponent =(props)=> shallow(<NotFound404 {...props}/>)
+
+describe('Should render NotFound404 component include',()=>{
+let component
+beforeEach(()=>{
+  component=getComponent()
+})
+  it('class .notFound404',()=>{
+    const wrapper = component.find('.notFound404')
+    expect(wrapper.length).toBe(1)
+    //console.log(component.debug())
+    })
+    
+    it('h1',()=>{
+      const wrapper = component.find('h1')
+      expect(wrapper.length).toBe(1)
+      })
+    it('SnapshotTest NotFound404 component',()=>{
+      const component = render(<NotFound404/>)
+      expect(component).toMatchSnapshot()
+    })  
+})
+
+
--- src/ui/components/main/notFound404/NotFound404.tsx
+++ src/ui/components/main/notFound404/NotFound404.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import s from './NotFound404.module.scss'
 
 export const NotFound404 = () => {
   return (
-    <div>
+    <div className={s.notFound404}>
       <h1>NOT FOUND 404</h1>
     </div>
   )
