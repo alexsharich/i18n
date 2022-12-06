5e8c764f5f8dedf431bfab3317a7c7f8dc578aca
alexsharich <alexsharich@gmail.com>
2022-12-06 01:35:01 +0100

    delete bad import

--- src/bll/auth/AuthReduces.ts
+++ src/bll/auth/AuthReduces.ts
@@ -1,5 +1,4 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { Console } from "console"
 import { i18nAPI, LoginParamsType } from "../../dal/i18nAPI"
 import { setLoaderThunk } from "../LoaderReducer"
 import { setIsModalActiveThunk } from "../ModalReducer"
@@ -11,35 +10,35 @@ const initialState = {
   token: "",
   updatedAt: "",
   __v: 0,
-  _id :  "",
-  initialized:false
+  _id: "",
+  initialized: false
 }
-export const authLoginThunk = createAsyncThunk('authLoginThunk',async(param:LoginParamsType,thunkAPI)=>{
+export const authLoginThunk = createAsyncThunk('authLoginThunk', async (param: LoginParamsType, thunkAPI) => {
   thunkAPI.dispatch(setIsModalActiveThunk(false))
   thunkAPI.dispatch(setLoaderThunk(true))
-  try{
-    const res  = await i18nAPI.login(param)
+  try {
+    const res = await i18nAPI.login(param)
     thunkAPI.dispatch(SetProfile(res.data))
-    thunkAPI.dispatch(IsInitialized({value:true}))
+    thunkAPI.dispatch(IsInitialized({ value: true }))
     thunkAPI.dispatch(setLoaderThunk(false))
-  }catch(err){
+  } catch (err) {
     console.log(err)
     thunkAPI.dispatch(setLoaderThunk(true))
   }
 })
 
 export const authSlice = createSlice({
-name:'authSlice',
-initialState,
-reducers:{
-  IsInitialized:(state,action:PayloadAction<{value:boolean}>)=>{
-    state.initialized = action.payload.value
-  },
-  SetProfile:(state,action:PayloadAction<any>)=>{
-     return action.payload
+  name: 'authSlice',
+  initialState,
+  reducers: {
+    IsInitialized: (state, action: PayloadAction<{ value: boolean }>) => {
+      state.initialized = action.payload.value
+    },
+    SetProfile: (state, action: PayloadAction<any>) => {
+      return action.payload
+    }
   }
-}
 })
 
 export const authReducer = authSlice.reducer
-export const {IsInitialized,SetProfile} = authSlice.actions
+export const { IsInitialized, SetProfile } = authSlice.actions
